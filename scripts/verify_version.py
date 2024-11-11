dc0cea19a78d7b7ff811a7fa9a24944ce8aa957f
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-11 17:16:47 +0100

    create python script verifying versions

---
+++ scripts/verify_version.py
@@ -0,0 +1,24 @@
+import duckdb
+import argparse
+
+# Verifying version
+parser = argparse.ArgumentParser()
+parser.add_argument("full_sha")
+parser.add_argument("--workflow")
+parser.add_argument("--architecture")
+args = parser.parse_args()
+if not args:
+    print("NO SHA SET")
+
+full_sha = args.full_sha
+workflow = args.workflow
+architecture = args.architecture
+
+short_sha=duckdb.sql("PRAGMA version").fetchone()[1]
+print(short_sha, args.full_sha)
+if args.full_sha.startswith(short_sha):
+    message = f"#### The version of `{ workflow }` build (`{ short_sha }`) is not the same as the version triggered the build (`{ full_sha }`).\n "
+    with open("issue_body_{}.txt".format(architecture), 'w') as f:
+        f.write(message)
+    print(message)
+
