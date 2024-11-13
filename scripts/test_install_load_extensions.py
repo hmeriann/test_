97cf30a259eee37be8c7b85c228fbc61b042d16d
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-13 16:11:59 +0100

    scripts/test_install_load_extensions.py

--- scripts/test_install_load_extensions.py
+++ scripts/test_install_load_extensions.py
@@ -4,11 +4,13 @@ import argparse
 # Verifying version
 parser = argparse.ArgumentParser()
 parser.add_argument("runs_on")
+parser.add_argument("--version")
 args = parser.parse_args()
 if not args:
     print("SET runs_on parameter")
 
 runs_on = args.runs_on
+version = args.version
 
 # TODO: if it is a release, check also "delta" (only for linux-python3) and "motherduck"
 extensions = [ "arrow", "autocomplete", "aws", "azure", "excel", "fts", "httpfs", "iceberg", "icu", "inet", "json",
@@ -18,37 +20,37 @@ with open("issue_body_Python_extensions_{}.txt".format(runs_on), 'w') as f:
     for extension in extensions:
         if extension == "unexpected":
             try:
-                duckdb.sql(f"INSTALL {extension}")
-                message = f"#### `{extension}` was unexpectedly installed.\n "
+                duckdb.sql(f"INSTALL { extension }")
+                message = f"#### `{ extension }` was unexpectedly installed on Python { runs_on }_{ version }.\n "
                 f.write(message)
                 print(message)
                 try:
-                    duckdb.sql(f"LOAD {extension}")
-                    message = f"#### `{extension}` was unexpectedly loaded.\n "
+                    duckdb.sql(f"LOAD { extension }")
+                    message = f"#### `{ extension }` was unexpectedly loaded on Python { runs_on }_{ version }.\n "
                     f.write(message)
                     print(message)
                 except Exception as e:
-                    print(f"Extension `{extension}` is not loaded\n")
+                    print(f"Extension `{ extension }` is not loaded\n")
                     pass
             
             except Exception as e:
-                print(f"Extension `{extension}` is not installed ")
+                print(f"Extension `{ extension }` is not installed ")
                 pass
 
         else:
             try:
-                duckdb.sql(f"INSTALL {extension}")
-                print(f"Installed {extension} ")
+                duckdb.sql(f"INSTALL { extension }")
+                print(f"Installed { extension } ")
 
                 try:
-                    duckdb.sql(f"LOAD {extension}")
-                    print(f"Loaded {extension} ")
+                    duckdb.sql(f"LOAD { extension }")
+                    print(f"Loaded { extension } ")
                 except Exception as e:
-                    message = f"#### Error loading `{extension}`: `{str(e)}`\n "
+                    message = f"- Error loading `{ extension }` Python on { runs_on }_{ version }: `{str(e)}`\n "
                     f.write(message)
                     print(message)
 
             except Exception as e:
-                message = f"#### Error installing `{extension}`: `{str(e)}`\n "
+                message = f"- Error installing `{ extension }` Python on { runs_on }_{ version }: `{str(e)}`\n "
                 f.write(message)
                 print(message)
