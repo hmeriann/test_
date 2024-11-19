be9ad4a78dcb09a7337765338f7e1b5045d12660
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-19 14:44:12 +0100

    scripts/test_install_load_extensions.py

--- scripts/test_install_load_extensions.py
+++ scripts/test_install_load_extensions.py
@@ -15,24 +15,27 @@ version = args.version
 
 # TODO: if it is a release, check also "delta" (only for linux-python3) and "motherduck"
 
-with open("extensions/.github/config/out_of_tree_extensions.cmake", "r") as file:
-    content = file.read()
+# with open("extensions/.github/config/out_of_tree_extensions.cmake", "r") as file:
+#     content = file.read()
 
-    pattern = r"duckdb_extension_load\(\s*([^\s,)]+)"
-    extensions = re.findall(pattern, content)
+#     pattern = r"duckdb_extension_load\(\s*([^\s,)]+)"
+#     extensions = re.findall(pattern, content)
 
-with open("issue_body_Python_extensions_{}.txt".format(runs_on), 'w') as f:
+extensions = [ 'one', 'two', 'three' ]
+
+with open("issue_body_Python_extensions.txt", 'w') as f:
+    f.write(f"runs_on,version,extension,failed_statement\n")
     for extension in extensions:
         if extension == "unexpected":
             try:
                 duckdb.sql(f"INSTALL { extension }")
                 message = f"#### `{ extension }` was unexpectedly installed on Python { runs_on }_{ version }.\n "
-                f.write(message)
+                f.write(f"`{ runs_on }`,`{ version }`,`{ extension }`,INSTALL\n")
                 print(message)
                 try:
                     duckdb.sql(f"LOAD { extension }")
                     message = f"#### `{ extension }` was unexpectedly loaded on Python { runs_on }_{ version }.\n "
-                    f.write(message)
+                    f.write(f"`{ runs_on }`,`{ version }`,`{ extension }`,LOAD\n")
                     print(message)
                 except Exception as e:
                     print(f"Extension `{ extension }` is not loaded\n")
@@ -52,10 +55,10 @@ with open("issue_body_Python_extensions_{}.txt".format(runs_on), 'w') as f:
                     print(f"Loaded { extension } ")
                 except Exception as e:
                     message = f"- Error loading `{ extension }` Python on { runs_on }_{ version }: `{str(e)}`\n "
-                    f.write(message)
+                    f.write(f"`{ runs_on }`,`{ version }`,`{ extension }`,INSTALL\n")
                     print(message)
 
             except Exception as e:
                 message = f"- Error installing `{ extension }` Python on { runs_on }_{ version }: `{str(e)}`\n "
-                f.write(message)
+                f.write(f"`{ runs_on }`,`{ version }`,`{ extension }`,LOAD\n")
                 print(message)
