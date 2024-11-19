db6f1fb5d5f531ab84bd4a0c561b2be753e50dc2
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-19 15:31:51 +0100

    scripts/prepare_report.py

---
+++ scripts/prepare_report.py
@@ -0,0 +1,22 @@
+import duckdb
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_name")
+args = parser.parse_args()
+if not args:
+    print("SET file_name argument")
+
+file_name = args.file_name
+
+with open("res.md". 'w') as f:
+    f.write(f"runs_on,version,extension,failed_statement\n")
+    f.write(f"----|----|----|----\n")
+    duckdb.sql("
+        .mode markdown
+        COPY (SELECT * FROM read_csv("{ file_name }")
+            GROUP BY extension, ORDER BY failed_statement)
+        TO tmp.md (HEADER 1, SEPARATOR '|')
+    ")
+    with open("tmp.md", 'r') as tbl:
+        f.write(tbl.read())
