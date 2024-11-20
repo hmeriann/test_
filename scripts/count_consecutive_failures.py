74d4f3e7d7d47e773747adf0d1f3ce770bcb577a
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-20 13:18:14 +0100

    fix the wrong quotes

--- scripts/count_consecutive_failures.py
+++ scripts/count_consecutive_failures.py
@@ -45,7 +45,7 @@ def create_issue_body():
                     SELECT conclusion, startedAt, url
                     FROM read_json('{ input_file }') 
                     WHERE conclusion='failure'
-                    LIMIT `{ failures }`
+                    LIMIT '{ failures }'
                 )  
                 TO '{ failures_list }' (HEADER 0, QUOTE '', SEPARATOR '|');
                 """)
