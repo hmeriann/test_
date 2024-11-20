ba3398856e6ca3d67f7c8e76460785a1e7ee3857
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-20 10:34:49 +0100

    .github/workflows/python_nbc.yml

--- scripts/test_install_load_extensions.py
+++ scripts/test_install_load_extensions.py
@@ -4,12 +4,14 @@ import re
 
 # Verifying version
 parser = argparse.ArgumentParser()
-parser.add_argument("runs_on")
+parser.add_argument("nightly_build")
+parser.add_argument("--runs_on")
 parser.add_argument("--version")
 args = parser.parse_args()
 if not args:
     print("SET runs_on parameter")
 
+nightly_build = args.nightly_build
 runs_on = args.runs_on
 version = args.version
 
@@ -23,18 +25,18 @@ version = args.version
 
 extensions = [ 'one', 'two', 'three' ]
 
-with open("issue_body_Python_extensions_{}.txt".format(runs_on), 'w') as f:
+with open("issue_body_extensions_{}.txt".format(nightly_build), 'w') as f:
     for extension in extensions:
         if extension == "unexpected":
             try:
                 duckdb.sql(f"INSTALL { extension }")
                 message = f"#### { extension } was unexpectedly installed on Python { runs_on }_{ version }.\n "
-                f.write(f"Python,{ runs_on },{ version },{ extension },INSTALL\n")
+                f.write(f"{ nightly_build },{ runs_on },{ version },{ extension },INSTALL\n")
                 print(message)
                 try:
                     duckdb.sql(f"LOAD { extension }")
                     message = f"#### { extension } was unexpectedly loaded on Python { runs_on }_{ version }.\n "
-                    f.write(f"Python,{ runs_on },{ version },{ extension },LOAD\n")
+                    f.write(f"{ nightly_build },{ runs_on },{ version },{ extension },LOAD\n")
                     print(message)
                 except Exception as e:
                     print(f"Extension { extension } is not loaded\n")
@@ -54,10 +56,10 @@ with open("issue_body_Python_extensions_{}.txt".format(runs_on), 'w') as f:
                     print(f"Loaded { extension } ")
                 except Exception as e:
                     message = f"- Error loading { extension } Python on { runs_on }_{ version }: {str(e)}\n "
-                    f.write(f"Python,{ runs_on },{ version },{ extension },INSTALL\n")
+                    f.write(f"{ nightly_build },{ runs_on },{ version },{ extension },INSTALL\n")
                     print(message)
 
             except Exception as e:
                 message = f"- Error installing { extension } Python on { runs_on }_{ version }: {str(e)}\n "
-                f.write(f"Python,{ runs_on },{ version },{ extension },LOAD\n")
+                f.write(f"{ nightly_build },{ runs_on },{ version },{ extension },LOAD\n")
                 print(message)
