76747860afbd88f9de4b7708a330f000099d60b1
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-22 16:47:39 +0100

    .github/workflows/tests_nbc_copy.yml

--- scripts/find_extensions.py
+++ scripts/find_extensions.py
@@ -3,11 +3,14 @@ import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("config")
+parser.add_argument("--arch")
+
 args = parser.parse_args()
 if not args:
     print("SET path to the out_of_tree_extensions.cmake file")
 
 config = args.config
+arch = args.arch
 
 with open(config, "r") as file:
     content = file.read()
@@ -16,3 +19,6 @@ with open(config, "r") as file:
     pattern = r"duckdb_extension_load\(\s*([^\s,)]+)"
     matches = re.findall(pattern, content)
     print(matches)
+
+if arch == arch.contains("aarch64"):
+    matches = matches.replace('[', '(').replace(']', ')')
