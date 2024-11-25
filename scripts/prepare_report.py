52da886a0f2d741fb318aa8212cddd9bec7ce47d
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-25 17:09:33 +0100

    getting run_url to the extension failures report

--- scripts/prepare_report.py
+++ scripts/prepare_report.py
@@ -4,15 +4,17 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("file_name")
 parser.add_argument("--platform")
+parser.add_argument("--url")
 args = parser.parse_args()
 if not args:
     print("SET file_name argument")
 
 file_name = args.file_name
 platform = args.platform
+url = args.url
 
 with open("res_{}.md".format(platform), 'w') as f:
-    f.write(f"\n#### Extensions failed to INSTALL or to LOAD:\n")
+    f.write(f"\n#### Extensions failed to INSTALL or to LOAD: [ Run Link ](https:'{ url }')\n")
     f.write(f" Nightly-build | Runs_on | Version | Extension | Failed statement \n")
     f.write(f"----|----|----|----|----\n")
     duckdb.sql(f"""
