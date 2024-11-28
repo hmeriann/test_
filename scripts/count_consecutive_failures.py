b4a4cdc816802b0e72929ab398c5abbb534f97cd
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-28 18:46:39 +0100

    scripts/count_consecutive_failures.py

--- scripts/count_consecutive_failures.py
+++ scripts/count_consecutive_failures.py
@@ -16,11 +16,9 @@ input_file = args.input_file
 nightly_build = args.nightly_build
 architecture = args.architecture
 url = args.url
-print(url, "🥵")
 all = duckdb.sql(f"SELECT * FROM read_json('{ input_file }')")
 rows = all.fetchall()
 conclusions = [row[0] for row in rows]
-print(conclusions)
 failures=0
 for c in conclusions:
     if c == 'failure':
@@ -29,35 +27,32 @@ for c in conclusions:
         break
 
 def create_issue_body():
-    failures_list = "failures_list.md"
-    duckdb.sql(f"""
-                COPY (
-                    SELECT conclusion, startedAt, url
-                    FROM read_json('{ input_file }') 
-                    WHERE conclusion='failure'
-                    LIMIT '{ failures }'
-                )  
-                TO '{ failures_list }' (HEADER 0, QUOTE '', SEPARATOR '|');
-                """)
-    with open("issue_body_{}.txt".format(architecture), 'w') as f:
-        f.write(f"At least one job had failed in the **'{ nightly_build }'** nightly-build run consecutively '{ failures }' times: [ Run Link ](https:{ url })\n")
-        f.write(f"#### Failure Details\n\n")
-        f.write(f"| Conclusion | Started At | Run URL |\n")
-        f.write(f"|------------|------------|---------|\n")
-        with open(failures_list, 'r') as failures_file:
-            f.write(failures_file.read())
-
-def main():
     if failures >= 1:
-        print(f"Found '{ failures }' failures.")
-        # ???: Do we want to report only when >= 4 consecutive failures?
-        # if failures >= 4:
-        create_issue_body()
+        failures_list = "failures_list.md"
+        duckdb.sql(f"""
+                    COPY (
+                        SELECT conclusion, createdAt, url
+                        FROM read_json('{ input_file }') 
+                        WHERE conclusion='failure'
+                        LIMIT '{ failures }'
+                    )  
+                    TO '{ failures_list }' (HEADER 0, QUOTE '', SEPARATOR '|');
+                    """)
+        with open("issue_body_{}.txt".format(architecture), 'w') as f:
+            f.write(f"The **'{ nightly_build }'** nightly-build has not succeeded the previous '{ failures }' times.\nSee the latest run: [ Run Link ](https:{ url })\n")
+            f.write(f"#### Failure Details\n\n")
+            f.write(f"| Conclusion | Started At | Run URL |\n")
+            f.write(f"|------------|------------|---------|\n")
+            with open(failures_list, 'r') as failures_file:
+                f.write(failures_file.read())
         return 1
     else:
-        print(f"No failures found.")
-
+        with open("issue_body_{}.txt".format(architecture), 'w') as f:
+            f.write(f"The **'{ nightly_build }'** nightly-build has succeeded.\nSee the latest run: [ Run Link ](https:{ url })\n")
         return 0
 
+def main():
+    create_issue_body()
+    
 if __name__ == "__main__":
     main()
