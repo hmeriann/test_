b9a4ab03c6fdee19a03d0ff5ef888d40ba32af4b
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-11-29 10:55:47 +0100

    .github/workflows/nrc_copy.yml

--- scripts/prepare_report.py
+++ scripts/prepare_report.py
@@ -12,16 +12,32 @@ if not args:
 file_name = args.file_name
 platform = args.platform
 url = args.url
+# with open("res_{}.md".format(platform), 'w') as f:
+#     f.write(f"\n#### Extensions failed to INSTALL or to LOAD: [ Run Link ](https:{ url })\n")
+#     f.write(f" Nightly-build | Architecture | Runs_on | Version | Extension | Failed statement \n")
+#     f.write(f"----|----|----|----|----|----\n")
+#     duckdb.sql(f"""
+#                 COPY (SELECT * 
+#                     FROM read_csv("{ file_name }")
+#                         ORDER BY nightly_build, architecture, runs_on, version, extension, failed_statement
+#                     )
+#                 TO tmp.md (HEADER 0, SEPARATOR '|')
+#                 """)
+#     with open("tmp.md", 'r') as tbl:
+#         f.write(tbl.read())
+
+
 with open("res_{}.md".format(platform), 'w') as f:
     f.write(f"\n#### Extensions failed to INSTALL or to LOAD: [ Run Link ](https:{ url })\n")
-    f.write(f" Nightly-build | Architecture | Runs_on | Version | Extension | Failed statement \n")
-    f.write(f"----|----|----|----|----|----\n")
-    duckdb.sql(f"""
-                COPY (SELECT * 
-                    FROM read_csv("{ file_name }")
-                        ORDER BY nightly_build, architecture, runs_on, version, extension, failed_statement
-                    )
-                TO tmp.md (HEADER 0, SEPARATOR '|')
-                """)
-    with open("tmp.md", 'r') as tbl:
-        f.write(tbl.read())
+    
+    
+    
+    
+    
+    f.write(duckdb.query(f"""
+                SELECT * 
+                FROM read_csv("{ file_name }")
+                    ORDER BY nightly_build, architecture, runs_on, version, extension, failed_statement
+                """).to_df().to_markdown()
+    )
+    
